df04cb897e26b892a4f906de024ad4821aca51f4
nazeer2k4 <153453454+nazeer2k4@users.noreply.github.com>
2025-08-13 21:56:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
             return callback(null, true);
         }
         // Allow your specific frontend (add more if needed)
-        if (origin === 'https://vintara-psi.vercel.app/' || origin === 'https://vintara2.vercel.app/') {
+        if (origin === 'https://vintara-psi.vercel.app/' || origin === 'https://vintara2.vercel.app/'|| origin ==='https://vintara-psi.vercel.app/formDetail.html') {
             return callback(null, true);
         }
         return callback(new Error('Not allowed by CORS'));
@@ -41,5 +41,6 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
